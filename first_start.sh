e9abdd0f3819ad6f7366c6d5a148997a8b49227b
july rise <julyrise@ya.ru>
2019-12-10 23:58:58 +0300

    upd

--- first_start.sh
+++ first_start.sh
@@ -2,13 +2,19 @@
 
 # Example usage: ./first_start.sh my_openvpn_server.com
 
-if [ ! $1]; then
+if [ ! $1 ]; then
     export ADDR=$(curl https://ipinfo.io/ip);
 else
     export ADDR=$1;
 fi
 
-echo "Set OpenVPN server address to $ADDR"
+if [ ! $2 ]; then
+    export PROTO=udp;
+else
+    export PROTO=$2;
+fi
 
-docker-compose run srv ovpn_genconfig -u udp://$ADDR;
+echo "Set OpenVPN server address to $ADDR and $PROTO"
+
+docker-compose run srv ovpn_genconfig -u $PROTO://$ADDR;
 docker-compose run srv ovpn_initpki;
--- remove-sert.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-source names.sh
-
-docker run --rm -it -v $OVPN_DATA:/etc/openvpn $OVPN_IMAGE ovpn_revokeclient $1 remove
